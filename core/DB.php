c1ddb4c68d6289055ffb01c96723fc02de8310ef
dfordavidai <tigerdavidguy@gmail.com>
2026-06-09 14:50:45 +0100

    Update MYSQL_ATTR_FOUND_ROWS initialization

--- core/DB.php
+++ core/DB.php
@@ -24,7 +24,7 @@ class DB {
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_EMULATE_PREPARES   => false,
                     PDO::ATTR_PERSISTENT         => false,
-                    PDO::MYSQL_ATTR_FOUND_ROWS   => true,
+                    PDO::MYSQL_ATTR_FOUND_ROWS   => defined('PDO::MYSQL_ATTR_FOUND_ROWS') ? true : false,
                 ]);
             } catch (PDOException $e) {
                 if (APP_DEBUG) {
